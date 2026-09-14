fa6fd1d4937f68ac910a1966adc0f91bd2afe302
Sedera10 <sederavalisoarandrianantenaina@gmail.com>
2026-09-14 20:03:05 +0300

    Initial commit

---
+++ src/main/java/mg/lagrace/api/LagraceApiApplication.java
@@ -0,0 +1,13 @@
+package mg.lagrace.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagraceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagraceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/lagrace/api/LagraceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.lagrace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagraceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
